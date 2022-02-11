206c5b0953e4c424570ecb8aef76761d56cfd34d
mariusleckebusch <marius.leckebusch@482-projects.com>
2022-02-11 11:45:44 +0100

    push

--- README.md
+++ README.md
@@ -13,7 +13,33 @@ Es dürfen immer nur maximal 2 "Personen" im Boot sitzen.
 
 Werden Wolf & Schaf bzw. Schaf und Kohlkopf nicht beaufsichtigt, kommt es zu einem unerwünschten Fressverhalten - das Spiel ist dann verloren weil einer der "Personen" leider tot ist.
 
-Lösung:
-1. Der Bauer muss mit dem Kohlkopf den Fluss überqueren
-2. Der Bauer muss mit dem Wolf den Fluss überqueren
-3. Der Bauer muss mit dem Schaf den Fluss überqueren
+Demo:
+https://mariusleckebusch.github.io/Aufgabe-4
+
+Steuerung:
+Bauer und Schaf: LEFT
+Bauer und Wolf: RIGHT
+Bauer und Kohlkopf: DOWN
+Bauer: UP
+<details>
+  <summary>Lösung</summary>
+
+1. Der Bauer überquert mit dem Schaf den Fluss und setzt es am anderen Ufer ab.
+2. Der Bauer rudert anschließend allein zurück.
+3. Der Bauer nimmt jetzt den Kohlkopf mit. Er setzt ihn am Ufer ab.
+4. Damit das Schaf sich nicht über den Kohlkopf hermacht, nimmt er das Schaf auf seinem Rückweg wieder mit.
+5. Der Bauer setzt jetzt das Schaf ab, nimmt den Wolf mit und lässt ihn am anderen Ufer mit dem Kohlkopf zurück.
+6. Der Bauer rudert wieder alleine zurück.
+7. Der Bauer nimmt schließlich das Schaf mit.
+
+<details>
+  <summary>Lösung Keys</summary>
+1. LEFT
+2. UP
+3. DOWN
+4. LEFT
+5. Right
+6. UP
+7. LEFT
+</details>
+</details>
---
+++ index.js
@@ -0,0 +1,153 @@
+var error = document.getElementById('error');
+var html_schaf = document.getElementById('schaf');
+var html_wolf = document.getElementById('wolf');
+var html_kohlkopf = document.getElementById('kohlkopf');
+var html_bauer = document.getElementById('bauer');
+var button_restart = document.getElementById('restart').onclick = function() { location.reload() };
+var keysenabled = true;
+function UpdateGameInfo() {
+    html_schaf.innerText = JSON.stringify(Schaf);
+    html_wolf.innerText = JSON.stringify(Wolf);
+    html_kohlkopf.innerText = JSON.stringify(Kohlkopf);
+    html_bauer.innerText = JSON.stringify(Bauer);
+}
+
+var Wolf = {
+    name: "Wolf",
+    age: 1,
+    position: "right"
+}
+
+var Schaf = {
+    name: "Schaf",
+    age: 1,
+    position: "right"
+}
+
+var Kohlkopf = {
+    name: "Kohlkopf",
+    age: 1,
+    position: "right"
+}
+
+var Bauer = {
+    name: "Bauer",
+    age: 1,
+    position: "right"
+}
+
+UpdateGameInfo()
+
+
+window.addEventListener('keydown', function(event) {
+    if(keysenabled) {
+    if(event.keyCode == 37) {
+        error.innerText = 'Left was pressed';
+        //Bauer und Schaf
+        if(Bauer.position != Schaf.position) {
+            error.innerText = 'Der Bauer ist nicht beim Schaf';
+        } else {
+            if(Bauer.position == "right" && Schaf.position == "right") {
+                Bauer.position = "left";
+                Schaf.position = "left";
+                Bauer.age = Bauer.age+1;
+                Schaf.age = Schaf.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+            } else {
+                Bauer.position = "right";
+                Schaf.position = "right";
+                Bauer.age = Bauer.age+1;
+                Schaf.age = Schaf.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+            }
+        }
+    }
+    if(event.keyCode == 39) {
+        error.innerText = 'Right was pressed';
+        //Bauer und Wolf
+        if(Bauer.position != Wolf.position) {
+            error.innerText = 'Der Bauer ist nicht beim Wolf';
+        } else {
+            if(Bauer.position == "right" && Wolf.position == "right") {
+                Bauer.position = "left";
+                Wolf.position = "left";
+                Bauer.age = Bauer.age+1;
+                Wolf.age = Wolf.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+            } else {
+                Bauer.position = "right";
+                Wolf.position = "right";
+                Bauer.age = Bauer.age+1;
+                Wolf.age = Wolf.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+            }
+        }
+    }
+    if(event.keyCode == 40) {
+        error.innerText = 'Down was pressed';
+        //Bauer und Kohlkopf
+        if(Bauer.position != Kohlkopf.position) {
+            error.innerText = 'Der Bauer ist nicht beim Kohlkopf';
+        } else {
+            if(Bauer.position == "right" && Kohlkopf.position == "right") {
+                Bauer.position = "left";
+                Kohlkopf.position = "left";
+                Bauer.age = Bauer.age+1;
+                Kohlkopf.age = Kohlkopf.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+            } else {
+                Bauer.position = "right";
+                Kohlkopf.position = "right";
+                Bauer.age = Bauer.age+1;
+                Kohlkopf.age = Kohlkopf.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+            }
+        }
+    }
+    if(event.keyCode == 38) {
+        error.innerText = 'Up was pressed';
+        //Bauer alleine
+            if(Bauer.position == "right" && Kohlkopf.position == "right") {
+                Bauer.position = "left";
+                Bauer.age = Bauer.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+            } else {
+                Bauer.position = "right";
+                Bauer.age = Bauer.age+1;
+                CheckLebewesen();
+                UpdateGameInfo();
+        }
+    }
+}
+});
+
+
+function CheckLebewesen() {
+if(Wolf.position == Schaf.position && Bauer.position !== Schaf.position) 
+{
+    error.innerText = "Der Wolf hat das Schaf gefressen, da der Bauer nicht aufgepasst hat!";
+    keysenabled = false;
+}
+if(Schaf.position == Kohlkopf.position && Bauer.position !== Kohlkopf.position) 
+{
+    error.innerText = "Das Schaf hat den Kohlkopf gefressen, da der Bauer nicht aufgepasst hat!";
+    keysenabled = false;
+}
+if(Wolf.position == "left" && Schaf.position == "left" && Kohlkopf.position == "left" && Bauer.position == "left")
+{
+    error.innerText = "Alle Lebewesen wurden erfolgreich auf das andere Ufer gebracht";
+    keysenabled = false;
+}
+if(Wolf.age == 15 || Bauer.age == 15 || Kohlkopf.age == 15 || Schaf.age == 15)
+{
+    error.innerText = "Ein Lebewesen ist gestorben da es zu Alt geworden ist";
+    keysenabled = false;
+}
+}
