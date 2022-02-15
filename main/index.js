bb3809f8cd4a0b0cd2e73177a2df331471fe2844
mariusleckebusch <marius.leckebusch@482-projects.com>
2022-02-15 09:34:25 +0100

    push

--- main/index.js
+++ main/index.js
@@ -3,10 +3,6 @@ var html_schaf = document.getElementById('schaf');
 var html_wolf = document.getElementById('wolf');
 var html_kohlkopf = document.getElementById('kohlkopf');
 var html_bauer = document.getElementById('bauer');
-var button_bauer_und_schaf = document.getElementById('bauerundschaf');
-var button_bauer_und_wolf = document.getElementById('bauerundwolf');
-var button_bauer_und_kohlkopf = document.getElementById('bauerundkohlkopf');
-var button_bauer = document.getElementById('bauer1');
 var button_restart = document.getElementById('restart').onclick = function () { location.reload() };
 var keysenabled = true;
 function UpdateGameInfo() {
@@ -41,190 +37,147 @@ var Bauer = {
 }
 
 UpdateGameInfo()
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function Teilaufgabe_5() {
+    setTimeout(() => {
+    BauerundSchaf() //left
+    error.innerText = "Left was Pressed"
+    }, 1000);
+    setTimeout(() => {
+    error.innerText = "Up was Pressed"
+    Bauer_Key() //up
+    }, 2000);
+    setTimeout(() => {
+    error.innerText = "Down was Pressed"
+    BauerundKohlkopf() //down
+    }, 3000);
+    setTimeout(() => {
+    error.innerText = "Left was Pressed"
+    BauerundSchaf() //left
+    }, 4000);
+    setTimeout(() => {
+    error.innerText = "Right was Pressed"
+    BauerundWolf() //right
+    }, 5000);
+    setTimeout(() => {
+    error.innerText = "Up was Pressed"
+    Bauer_Key() //up
+    }, 6000);
+    setTimeout(() => {
+    error.innerText = "Left was Pressed"
+    BauerundSchaf() //left
+    }, 7000);
+}
 
+Teilaufgabe_5()
+
+function BauerundSchaf() {
+    if (Bauer.position != Schaf.position) {
+        error.innerText = 'Der Bauer ist nicht beim Schaf';
+    } else {
+        if (Bauer.position == "right" && Schaf.position == "right") {
+            Bauer.position = "left";
+            Schaf.position = "left";
+            Bauer.age = Bauer.age + 1;
+            Schaf.age = Schaf.age + 1;
+            CheckLebewesen();
+            UpdateGameInfo();
+        } else {
+            Bauer.position = "right";
+            Schaf.position = "right";
+            Bauer.age = Bauer.age + 1;
+            Schaf.age = Schaf.age + 1;
+            CheckLebewesen();
+            UpdateGameInfo();
+        }
+    }
+}
+
+function BauerundWolf() {
+    if (Bauer.position != Wolf.position) {
+        error.innerText = 'Der Bauer ist nicht beim Wolf';
+    } else {
+        if (Bauer.position == "right" && Wolf.position == "right") {
+            Bauer.position = "left";
+            Wolf.position = "left";
+            Bauer.age = Bauer.age + 1;
+            Wolf.age = Wolf.age + 1;
+            CheckLebewesen();
+            UpdateGameInfo();
+        } else {
+            Bauer.position = "right";
+            Wolf.position = "right";
+            Bauer.age = Bauer.age + 1;
+            Wolf.age = Wolf.age + 1;
+            CheckLebewesen();
+            UpdateGameInfo();
+        }
+    }
+}
+
+function BauerundKohlkopf() {
+    if (Bauer.position != Kohlkopf.position) {
+        error.innerText = 'Der Bauer ist nicht beim Kohlkopf';
+    } else {
+        if (Bauer.position == "right" && Kohlkopf.position == "right") {
+            Bauer.position = "left";
+            Kohlkopf.position = "left";
+            Bauer.age = Bauer.age + 1;
+            Kohlkopf.age = Kohlkopf.age + 1;
+            CheckLebewesen();
+            UpdateGameInfo();
+        } else {
+            Bauer.position = "right";
+            Kohlkopf.position = "right";
+            Bauer.age = Bauer.age + 1;
+            Kohlkopf.age = Kohlkopf.age + 1;
+            CheckLebewesen();
+            UpdateGameInfo();
+        }
+    }
+}
+
+function Bauer_Key() {
+    if (Bauer.position == "right" && Kohlkopf.position == "right") {
+        Bauer.position = "left";
+        Bauer.age = Bauer.age + 1;
+        CheckLebewesen();
+        UpdateGameInfo();
+    } else {
+        Bauer.position = "right";
+        Bauer.age = Bauer.age + 1;
+        CheckLebewesen();
+        UpdateGameInfo();
+    }
+}
 
 window.addEventListener('keydown', function (event) {
     if (keysenabled) {
         if (event.keyCode == 37) {
             error.innerText = 'Left was pressed';
             //Bauer und Schaf
-            if (Bauer.position != Schaf.position) {
-                error.innerText = 'Der Bauer ist nicht beim Schaf';
-            } else {
-                if (Bauer.position == "right" && Schaf.position == "right") {
-                    Bauer.position = "left";
-                    Schaf.position = "left";
-                    Bauer.age = Bauer.age + 1;
-                    Schaf.age = Schaf.age + 1;
-                    CheckLebewesen();
-                    UpdateGameInfo();
-                } else {
-                    Bauer.position = "right";
-                    Schaf.position = "right";
-                    Bauer.age = Bauer.age + 1;
-                    Schaf.age = Schaf.age + 1;
-                    CheckLebewesen();
-                    UpdateGameInfo();
-                }
-            }
+            BauerundSchaf();
         }
         if (event.keyCode == 39) {
             error.innerText = 'Right was pressed';
             //Bauer und Wolf
-            if (Bauer.position != Wolf.position) {
-                error.innerText = 'Der Bauer ist nicht beim Wolf';
-            } else {
-                if (Bauer.position == "right" && Wolf.position == "right") {
-                    Bauer.position = "left";
-                    Wolf.position = "left";
-                    Bauer.age = Bauer.age + 1;
-                    Wolf.age = Wolf.age + 1;
-                    CheckLebewesen();
-                    UpdateGameInfo();
-                } else {
-                    Bauer.position = "right";
-                    Wolf.position = "right";
-                    Bauer.age = Bauer.age + 1;
-                    Wolf.age = Wolf.age + 1;
-                    CheckLebewesen();
-                    UpdateGameInfo();
-                }
-            }
+            BauerundWolf();
         }
         if (event.keyCode == 40) {
             error.innerText = 'Down was pressed';
             //Bauer und Kohlkopf
-            if (Bauer.position != Kohlkopf.position) {
-                error.innerText = 'Der Bauer ist nicht beim Kohlkopf';
-            } else {
-                if (Bauer.position == "right" && Kohlkopf.position == "right") {
-                    Bauer.position = "left";
-                    Kohlkopf.position = "left";
-                    Bauer.age = Bauer.age + 1;
-                    Kohlkopf.age = Kohlkopf.age + 1;
-                    CheckLebewesen();
-                    UpdateGameInfo();
-                } else {
-                    Bauer.position = "right";
-                    Kohlkopf.position = "right";
-                    Bauer.age = Bauer.age + 1;
-                    Kohlkopf.age = Kohlkopf.age + 1;
-                    CheckLebewesen();
-                    UpdateGameInfo();
-                }
-            }
+            BauerundKohlkopf();
         }
         if (event.keyCode == 38) {
             error.innerText = 'Up was pressed';
             //Bauer alleine
-            if (Bauer.position == "right" && Kohlkopf.position == "right") {
-                Bauer.position = "left";
-                Bauer.age = Bauer.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            } else {
-                Bauer.position = "right";
-                Bauer.age = Bauer.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            }
+            Bauer_Key();
         }
     }
 });
 
-button_bauer_und_schaf.onclick = function () {
-    if (keysenabled) {
-        if (Bauer.position != Schaf.position) {
-            error.innerText = 'Der Bauer ist nicht beim Schaf';
-        } else {
-            if (Bauer.position == "right" && Schaf.position == "right") {
-                Bauer.position = "left";
-                Schaf.position = "left";
-                Bauer.age = Bauer.age + 1;
-                Schaf.age = Schaf.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            } else {
-                Bauer.position = "right";
-                Schaf.position = "right";
-                Bauer.age = Bauer.age + 1;
-                Schaf.age = Schaf.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            }
-        }
-
-    } else {
-        button_bauer_und_schaf.disabled = true;
-    }
-}
-button_bauer_und_wolf.onclick = function () {
-    if (keysenabled) {
-        if (Bauer.position != Wolf.position) {
-            error.innerText = 'Der Bauer ist nicht beim Wolf';
-        } else {
-            if (Bauer.position == "right" && Wolf.position == "right") {
-                Bauer.position = "left";
-                Wolf.position = "left";
-                Bauer.age = Bauer.age + 1;
-                Wolf.age = Wolf.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            } else {
-                Bauer.position = "right";
-                Wolf.position = "right";
-                Bauer.age = Bauer.age + 1;
-                Wolf.age = Wolf.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            }
-        }
-    } else {
-        button_bauer_und_wolf.disabled = true;
-    }
-}
-button_bauer_und_kohlkopf.onclick = function () {
-    if (keysenabled) {
-        if (Bauer.position != Kohlkopf.position) {
-            error.innerText = 'Der Bauer ist nicht beim Kohlkopf';
-        } else {
-            if (Bauer.position == "right" && Kohlkopf.position == "right") {
-                Bauer.position = "left";
-                Kohlkopf.position = "left";
-                Bauer.age = Bauer.age + 1;
-                Kohlkopf.age = Kohlkopf.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            } else {
-                Bauer.position = "right";
-                Kohlkopf.position = "right";
-                Bauer.age = Bauer.age + 1;
-                Kohlkopf.age = Kohlkopf.age + 1;
-                CheckLebewesen();
-                UpdateGameInfo();
-            }
-        }
-    } else {
-        button_bauer_und_kohlkopf.disabled = true;
-    }
-}
-button_bauer.onclick = function () {
-    if (keysenabled) {
-        if (Bauer.position == "right" && Kohlkopf.position == "right") {
-            Bauer.position = "left";
-            Bauer.age = Bauer.age + 1;
-            CheckLebewesen();
-            UpdateGameInfo();
-        } else {
-            Bauer.position = "right";
-            Bauer.age = Bauer.age + 1;
-            CheckLebewesen();
-            UpdateGameInfo();
-        }
-    } else {
-        button_bauer.disabled = true;
-    }
-}
 function CheckLebewesen() {
     if (Wolf.position == Schaf.position && Bauer.position !== Schaf.position) {
         error.innerText = "Der Wolf hat das Schaf gefressen, da der Bauer nicht aufgepasst hat!";
@@ -238,7 +191,7 @@ function CheckLebewesen() {
         error.innerText = "Alle Lebewesen wurden erfolgreich auf das andere Ufer gebracht";
         keysenabled = false;
     }
-    if (Wolf.age == 15 || Bauer.age == 15 || Kohlkopf.age == 15 || Schaf.age == 15) {
+    if (Wolf.age == 1500 || Bauer.age == 1500 || Kohlkopf.age == 1500 || Schaf.age == 1500) {
         error.innerText = "Ein Lebewesen ist gestorben da es zu Alt geworden ist";
         keysenabled = false;
     }
