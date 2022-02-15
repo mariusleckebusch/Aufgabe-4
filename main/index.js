d9b1e7bfbfcfd3563394e8fd2c68493462321fbe
mariusleckebusch <marius.leckebusch@482-projects.com>
2022-02-15 11:01:53 +0100

    added buttons and Automatic Mode

--- main/index.js
+++ main/index.js
@@ -4,6 +4,11 @@ var html_wolf = document.getElementById('wolf');
 var html_kohlkopf = document.getElementById('kohlkopf');
 var html_bauer = document.getElementById('bauer');
 var button_restart = document.getElementById('restart').onclick = function () { location.reload() };
+var button_bauer_und_schaf = document.getElementById('bauerundschaf');
+var button_bauer_und_wolf = document.getElementById('bauerundwolf');
+var button_bauer_und_kohlkopf = document.getElementById('bauerundkohlkopf');
+var button_bauer = document.getElementById('bauer1');
+var button_auto = document.getElementById('auto').onclick = function () { Teilaufgabe_5() };
 var keysenabled = true;
 function UpdateGameInfo() {
     html_schaf.innerText = JSON.stringify(Schaf);
@@ -37,41 +42,49 @@ var Bauer = {
 }
 
 UpdateGameInfo()
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+
+
+
 function Teilaufgabe_5() {
+    Bauer.age = 1;
+    Bauer.position = "right";
+    Kohlkopf.age = 1;
+    Kohlkopf.position = "right";
+    Schaf.age = 1;
+    Schaf.position = "right";
+    Wolf.age = 1;
+    Wolf.position = "right";
+    UpdateGameInfo()
+    error.innerText = "Automode Starting...";
     setTimeout(() => {
-    BauerundSchaf() //left
-    error.innerText = "Left was Pressed"
+        BauerundSchaf() //left
+        error.innerText = "Left was Pressed"
     }, 1000);
     setTimeout(() => {
-    error.innerText = "Up was Pressed"
-    Bauer_Key() //up
+        error.innerText = "Up was Pressed"
+        Bauer_Key() //up
     }, 2000);
     setTimeout(() => {
-    error.innerText = "Down was Pressed"
-    BauerundKohlkopf() //down
+        error.innerText = "Down was Pressed"
+        BauerundKohlkopf() //down
     }, 3000);
     setTimeout(() => {
-    error.innerText = "Left was Pressed"
-    BauerundSchaf() //left
+        error.innerText = "Left was Pressed"
+        BauerundSchaf() //left
     }, 4000);
     setTimeout(() => {
-    error.innerText = "Right was Pressed"
-    BauerundWolf() //right
+        error.innerText = "Right was Pressed"
+        BauerundWolf() //right
     }, 5000);
     setTimeout(() => {
-    error.innerText = "Up was Pressed"
-    Bauer_Key() //up
+        error.innerText = "Up was Pressed"
+        Bauer_Key() //up
     }, 6000);
     setTimeout(() => {
-    error.innerText = "Left was Pressed"
-    BauerundSchaf() //left
+        error.innerText = "Left was Pressed"
+        BauerundSchaf() //left
     }, 7000);
 }
-
-Teilaufgabe_5()
 
 function BauerundSchaf() {
     if (Bauer.position != Schaf.position) {
@@ -153,6 +166,30 @@ function Bauer_Key() {
     }
 }
 
+
+button_bauer_und_schaf.onclick = function () {
+    if (keysenabled) {
+        BauerundSchaf();
+    }
+}
+button_bauer_und_wolf.onclick = function () {
+    if (keysenabled) {
+        BauerundWolf();
+    }
+}
+button_bauer_und_kohlkopf.onclick = function () {
+    if (keysenabled) {
+        BauerundKohlkopf();
+    }
+}
+button_bauer.onclick = function () {
+    if (keysenabled) {
+        Bauer_Key();
+    }
+}
+
+
+
 window.addEventListener('keydown', function (event) {
     if (keysenabled) {
         if (event.keyCode == 37) {
@@ -191,7 +228,7 @@ function CheckLebewesen() {
         error.innerText = "Alle Lebewesen wurden erfolgreich auf das andere Ufer gebracht";
         keysenabled = false;
     }
-    if (Wolf.age == 1500 || Bauer.age == 1500 || Kohlkopf.age == 1500 || Schaf.age == 1500) {
+    if (Wolf.age == 10 || Bauer.age == 10 || Kohlkopf.age == 10 || Schaf.age == 10) {
         error.innerText = "Ein Lebewesen ist gestorben da es zu Alt geworden ist";
         keysenabled = false;
     }
